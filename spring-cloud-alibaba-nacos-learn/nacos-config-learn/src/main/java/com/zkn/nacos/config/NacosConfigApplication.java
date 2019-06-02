39c532d4f092a24c5c3310118c1355e1a9702174
zhangkenan <zhangkenan@yunlizhihui.com>
2019-06-02 23:21:26 +0800

    初始化应用

---
+++ spring-cloud-alibaba-nacos-learn/nacos-config-learn/src/main/java/com/zkn/nacos/config/NacosConfigApplication.java
@@ -0,0 +1,18 @@
+package com.zkn.nacos.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author conanzhang@木森
+ * @description
+ * @date 2019-06-02 23:11
+ * @classname NacosConfigApplication
+ */
+@SpringBootApplication
+public class NacosConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigApplication.class);
+    }
+}
